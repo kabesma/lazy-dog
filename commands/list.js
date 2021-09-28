ca266fe7e97d5a2264c31acca80ceee954b3cd22
akilsagitarius <akil.dcc@gmail.com>
2021-09-28 16:12:49 +0800

    first project make command

---
+++ commands/list.js
@@ -0,0 +1,14 @@
+//const conf = new (require('conf'))()
+
+function list () {
+    console.log("welcome todo list");
+}
+
+function saya() {
+    console.log("ini saya");
+}
+
+module.exports = {
+    list,
+    saya
+};
