16417e2fab34935df2e3fe4453700ac5e9543d35
akilsagitarius <akil.dcc@gmail.com>
2021-09-29 15:37:55 +0800

    fixing request command

--- commands/executioner.js
+++ commands/executioner.js
@@ -1,12 +1,50 @@
 const chalk = require('chalk')
 const CLI = require('clui')
 const Spinner = CLI.Spinner
+const fs = require('fs')
+const { spawn } = require('child_process')
+let data = ''
 //const conf = new (require('conf'))()
 
-function make(source, destination, options) {
-    console.log(chalk.blue(options.cheese + " welcome " + source + " in  " + destination))
+/**
+ * Command make controller, view , and model
+ *
+ * @param {[string]} source - [you can write model, view, or controller]
+ * @param {[string]} destination - [location path for source]
+ * @param {[string]} options - [you can write all source with options]
+ * @return {[string]} [result from execute you file request]
+ */
+async function make(source, destination, options) {
+//     console.log(chalk.green(options.cheese + " welcome " + source + " in  " + destination))
+    let waiting = new Spinner(
+        'Exiting in 10 seconds...  ', 
+        [chalk.red('⣾'),chalk.red('⣽'),chalk.red('⣻'),chalk.red('⢿'),chalk.red('⡿'),chalk.red('⣟'),chalk.red('⣯'),chalk.red('⣷')]
+    )
+    waiting.start()
+    var number = 10
+    data = fs.readFileSync(destination + source, 'utf8')
+    setInterval(function () {
+        number--
+        waiting.message('Exiting in ' + number + ' seconds...  ')
+        if (number === 0) {
+            waiting.stop(); 
+            process.stdout.write('\n')
+            
+            try {
+                console.log(data)
+            } catch (err) {
+                console.error(err)
+            } 
+            process.exit(0)
+        }
+    }, 1000);
 }
 
+/**
+ * [TODO:description]
+ *
+ * @return {[TODO:type]} [TODO:description]
+ */
 function resource() {
     console.log("ini saya")
 }
