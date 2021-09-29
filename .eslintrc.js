2c71e459e6ed95a12a83c6871906d8d9929c077f
akilsagitarius <akil.dcc@gmail.com>
2021-09-29 10:31:27 +0800

    remove swp and setting eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,43 @@
 module.exports = {
+  'extends': [
+    'eslint:recommended',
+    'google',
+    'plugin:react/recommended',
+  ],
+  'parser': 'babel-eslint',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true,
+      'modules': true,
+    },
+  },
   'env': {
     'browser': true,
-    'es2021': true,
-  },
-  'extends': [
-    'google',
-  ],
-  'parserOptions': {
-    'ecmaVersion': 12,
-    'sourceType': 'module',
+    'node': true,
+    'es6': true,
   },
   'rules': {
+    'semi': [2, 'never'],
+    'indent': ['error',
+      2, {
+        'SwitchCase': 1,
+        'VariableDeclarator': 1,
+      },
+    ],
   },
-};
+}
+// module.exports = {
+//  'env': {
+//    'browser': true,
+//    'es2021': true,
+//  },
+//  'extends': [
+//    'google',
+//  ],
+//  'parserOptions': {
+//    'ecmaVersion': 12,
+//    'sourceType': 'module',
+//  },
+//  'rules': {
+//  },
+// };
